cc4cfc9fcf31e78470d14e114cc471c0ebcede3a
RodResq <rodrigoresque@yahoo.com.br>
2023-02-08 22:25:08 -0300

    updt

--- src/main/java/com/rodrigo/crudspring/controller/CourseController.java
+++ src/main/java/com/rodrigo/crudspring/controller/CourseController.java
@@ -2,8 +2,12 @@ package com.rodrigo.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rodrigo.crudspring.model.Course;
@@ -23,5 +27,10 @@ public class CourseController {
     public List<Course> list() {
         return this.courseRepository.findAll();
     }
-    
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
+    }
 }
